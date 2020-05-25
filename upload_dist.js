0bcb0baf2aeb10160cbd66f6fb7ef94ec7f3e3b3
Delivator <info@delivator.me>
2020-05-25 18:00:59 +0200

    Added skynet.local to upload dist script

--- upload_dist.js
+++ upload_dist.js
@@ -1,10 +1,11 @@
 const skynet = require("@nebulous/skynet");
 
 const portals = [
+  "http://skynet.local"
   "https://skyportal.xyz",
-  "https://skynet.cloudloop.io",
-  "https://siasky.net",
-  "https://skynethub.io",
+  // "https://skynet.cloudloop.io",
+  // "https://siasky.net",
+  // "https://skynethub.io",
 ];
 
 let opts = skynet.DefaultUploadOptions;
