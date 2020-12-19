551c924da4871a57d4bd9a68060f8056559fe1b7
Delivator <info@delivator.me>
2020-12-19 14:20:23 +0100

    Added skyid state to vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,62 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import SkyID from "skyid";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+const skyid = new SkyID("my-testapp", skyidCallback, skyidOptions);
+
+const skyidOptions = {
+  devMode:
+    window.location.hostname == "idtest.local" ||
+    window.location.hostname == "localhost" ||
+    window.location.protocol == "file:",
+};
+
+function skyidCallback(message) {
+  switch (message) {
+    case "login_fail":
+      console.log("Login failed");
+      break;
+    case "login_success":
+      skyid.getProfile((data) => {
+        if (data) {
+          console.log(data);
+          store.commit("setLoggedInUser", data);
+        } else {
+          store.commit("setLoggedInUser", "Anonym User");
+        }
+        console.log(data);
+      });
+      break;
+    case "destroy":
+      store.commit("setLoggedInUser", null);
+      break;
+    default:
+      console.log(message);
+      break;
+  }
+}
+
+const store = new Vuex.Store({
+  state: {
+    loggedInUser: null,
+  },
+  mutations: {
+    setLoggedInUser: (state, user) => {
+      state.loggedInUser = user;
+    },
+  },
+  actions: {
+    logInUser() {
+      skyid.sessionStart();
+    },
+
+    logOutUser() {
+      skyid.sessionDestroy();
+    },
+  },
   modules: {},
 });
+
+export default store;
